7e1c88df59ed757c0a2fe00bdc0ebd3dea8dabfd
Isaac Sears <isaac.j.sears@gmail.com>
2023-07-17 16:05:11 -0400

    Update patient dataset for new data format

--- physionet2023/dataProcessing/datasets.py
+++ physionet2023/dataProcessing/datasets.py
@@ -8,8 +8,8 @@ import torch
 from scipy.signal import decimate
 from sklearn.model_selection import train_test_split
 
+from helper_code import load_challenge_data, load_recording_data, load_text_file
 from physionet2023 import LabelType, config
-from physionet2023.dataProcessing.exampleUtil import *
 
 
 class PatientDataset(torch.utils.data.Dataset):
@@ -83,46 +83,6 @@ class PatientDataset(torch.utils.data.Dataset):
         else:
             self.features_dim = len(self.channels)
 
-        # Do quality control to ensure all patients have usable data based on quality cutoff
-        passed_qc = list()
-
-        for pid in self.patient_ids:
-            recording_metadata = self._load_recording_metadata(pid)
-            if len(recording_metadata) > 0:
-                passed_qc.append(pid)
-
-        self.patient_ids = passed_qc
-
-    def _load_recording_metadata(self, patient_id) -> pd.DataFrame:
-        recording_metadata_file = os.path.join(
-            self.root_folder, patient_id, patient_id + ".tsv"
-        )
-        recording_metadata = pd.read_csv(recording_metadata_file, delimiter="\t")
-        recording_metadata = recording_metadata.dropna()
-        recording_metadata = recording_metadata[
-            recording_metadata["Quality"] > self.quality_cutoff
-        ]
-
-        return recording_metadata
-
-    def _load_single_recording(self, patient_id, recording_id):
-        recording_location = os.path.join(self.root_folder, patient_id, recording_id)
-
-        recording_data, sampling_frequency, channels = load_recording(
-            recording_location
-        )
-
-        # Making some assumptions about uniformity of data. Will have to rethink this if not true
-        # ...seems like it is true; could take this out if performance becomes a concern
-        assert sampling_frequency == self.sampling_frequency
-        assert len(channels) == len(self.channels)
-        assert recording_data.shape[-1] == self.full_record_len
-
-        for expected_channel, actual_channel in zip(self.channels, channels):
-            assert expected_channel == actual_channel
-
-        return recording_data
-
     def _load_patient_metadata(self, patient_id) -> dict:
         # Define file location.
         patient_metadata_file = os.path.join(
@@ -145,18 +105,16 @@ class PatientDataset(torch.utils.data.Dataset):
         patient_id = self.patient_ids[index]
 
         patient_metadata = self._load_patient_metadata(patient_id)
-        recording_metadata = self._load_recording_metadata(patient_id)
+
+        with open(f"{self.root_folder}/{patient_id}/RECORDS", "r") as f:
+            records = [r.strip() for r in f.readlines() if "ECG" not in r]
 
         # Load recordings.
-        recordings = [
-            self._load_single_recording(patient_id, r)
-            for r in recording_metadata["Record"].to_list()
-        ]
+        # recordings = [
+        #     load_recording_data(f"{self.root_folder}/{patient_id}/{r}") for r in records
+        # ]
 
-        # Make sure each recording has associated metadata
-        assert len(recording_metadata) == len(recordings)
-
-        return patient_metadata, recording_metadata, recordings
+        return patient_metadata, records
 
     def _get_label(self, patient_id: str):
         if self.label_type == LabelType.DUMMY:
@@ -186,62 +144,6 @@ class PatientDataset(torch.utils.data.Dataset):
             raise ValueError(f"Unsupported label type: {self.label_type}")
 
 
-class PatientTrainingDataset(PatientDataset):
-    def __init__(self, root_folder: str, sample_len: int, normalize=True, **kwargs):
-        super().__init__(root_folder, **kwargs)
-
-        if self.include_static:
-            raise NotImplementedError
-
-        assert sample_len <= self.full_record_len
-
-        self.sample_len = sample_len
-        self.normalize = normalize
-
-    def collate(self, batch):
-        X = torch.stack([recording_data for recording_data, _ in batch], dim=0)
-        y = torch.stack([label for _, label in batch], dim=0)
-
-        return X, y
-
-    def tst_collate(self, batch):
-        """
-        TST also needs pad_mask, even though all sequences are the same length
-        """
-        X, y = self.collate(batch)
-
-        pad_mask = torch.ones_like(X[:, 0, :]).bool()
-
-        return X.permute(0, 2, 1), y, pad_mask, "DummyID"
-
-    def __getitem__(self, index: int):
-        patient_id = self.patient_ids[index]
-        patient_metadata = self._load_patient_metadata(patient_id)
-        recording_metadata = self._load_recording_metadata(patient_id)
-
-        # Only the finest recordings will do
-        # TODO: could iterate through all recordings and select the one with highest StD or something
-        # but that would be much more computationally expensive
-        recording_id = recording_metadata.nlargest(1, "Quality")["Record"].item()
-
-        recording_data = self._load_single_recording(patient_id, recording_id)
-
-        if self.normalize:
-            recording_data = (
-                recording_data - recording_data.mean()
-            ) / recording_data.std()
-
-        # Take sequence from the middle of the recording
-        left_margin = int((self.full_record_len - self.sample_len) / 2)
-        right_margin = (self.full_record_len - self.sample_len) - left_margin
-
-        recording_sample = recording_data[
-            :, left_margin : (left_margin + self.sample_len)
-        ]
-
-        return (torch.tensor(recording_sample), self._get_label(patient_id))
-
-
 def demo(dl, n_batches=3):
     for batchnum, batch_data in enumerate(dl):
         print("=" * 15)
@@ -253,4 +155,9 @@ def demo(dl, n_batches=3):
 
 
 if __name__ == "__main__":
-    pass
+    ds = PatientDataset()
+
+    for patient_metadata, recordings in ds:
+        print(patient_metadata)
+        print(recordings)
+        break
